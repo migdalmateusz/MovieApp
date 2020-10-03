924f3157ee955e452e3f68238259c9abb308bda0
Mateusz <migdalmateusz@wp.pl>
2020-10-03 12:30:37 +0200

    Create Map movieRepository

--- src/main/java/com/sda/MovieApp/controlers/Create.java
+++ src/main/java/com/sda/MovieApp/controlers/Create.java
@@ -2,17 +2,20 @@ package com.sda.MovieApp.controlers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
+@RestController
 public class Create
 {
-    public static List<Movie> movieRepository = new ArrayList<>();
+    public static Map<Integer, Movie> movieRepository = new HashMap();
+    public Integer currentId = 0;
     @PostMapping("/movies")
-    public void createMovie(@RequestBody String title, Integer id )
+    public void createMovie(@RequestBody Movie movie)
     {
-        Movie movie = new Movie("Top Gun", 1);
-        movieRepository.add(movie);
+        movieRepository.put(currentId, movie);
+        currentId = currentId + 1;
     }
 }
--- src/main/java/com/sda/MovieApp/controlers/ReadController.java
+++ src/main/java/com/sda/MovieApp/controlers/ReadController.java
@@ -1,14 +1,14 @@
 package com.sda.MovieApp.controlers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ReadController {
 
     @GetMapping("/movies/{id}")
-    public Movie readMovie (@RequestParam(value = "id") Integer id) {
+    public Movie readMovie (@PathVariable(value = "id") Integer id) {
         return Create.movieRepository.get(id);
     }
 }
