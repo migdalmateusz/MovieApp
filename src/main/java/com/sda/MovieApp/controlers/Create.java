f86298efd6735d70da12ecba55c69242f980355e
davew292 <davew@tuta.io>
2020-10-03 10:58:38 +0200

    createMovie()

--- src/main/java/com/sda/MovieApp/controlers/Create.java
+++ src/main/java/com/sda/MovieApp/controlers/Create.java
@@ -3,11 +3,16 @@ package com.sda.MovieApp.controlers;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Create
 {
+    List<Movie> movieRepository = new ArrayList<>();
     @PostMapping("/movies")
     public void createMovie(@RequestBody String title, Integer id )
     {
-        Movie movie = new Movie("Top Gun");
+        Movie movie = new Movie("Top Gun", 1);
+        movieRepository.add(movie);
     }
 }
